4c772e1cb442fd6bcff4c3b3d99f85c09c47ba19
김민우 <0chickim@gmail.com>
2024-06-13 22:36:47 +0900

    feat: imask 사용

--- src/pages/mask.page.tsx
+++ src/pages/mask.page.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
+import { IMaskInput } from 'react-imask'
 import { formatPhoneNumber } from '../features/mask/model/formatter'
 import { FormatInput } from '../features/mask/ui/format-input'
 import { PatternInput } from '../features/mask/ui/pattern-input'
@@ -6,6 +7,11 @@ import { PatternInput } from '../features/mask/ui/pattern-input'
 export const MaskPage = () => {
   const [value, setValue] = useState('')
   const [patternValue, setPatternValue] = useState('')
+  const [imaskValue, setIMaskValue] = useState('')
+
+  const ref = useRef(null)
+  const inputRef = useRef(null)
+
   return (
     <div>
       <h1>입력 마스킹</h1>
@@ -29,6 +35,16 @@ export const MaskPage = () => {
       />
 
       <h2>외부 라이브러리를 활용</h2>
+
+      <IMaskInput
+        value={imaskValue}
+        mask={'000-000-0000'}
+        unmask={true} // true|false|'typed'
+        ref={ref}
+        inputRef={inputRef} // access to nested input
+        placeholder="Enter number here"
+        onAccept={(value: string) => setIMaskValue(value)}
+      />
 
       <h2>외부 라이브러리와 UI 라이브러리를 활용하여 마스킹을 구현</h2>
     </div>
